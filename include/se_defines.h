7cb9034e9df5ba7f57578bc27652e4196394b466
peter <shiqingpeng@haier.com>
2023-06-26 14:50:02 +0800

    引擎接口定义

---
+++ include/se_defines.h
@@ -0,0 +1,41 @@
+/**
+ * @defgroup
+ * @{
+ * @copyright Copyright (c) 2023, Haier.Co, Ltd.
+ * @file se_defines.h
+ * @author shiqingpeng (shiqingpeng@haier.com)
+ * @brief 
+ * @date 2023-06-26
+ * 
+ * @par History:
+ * <table>
+ * <tr><th>Date         <th>version <th>Author  <th>Description
+ * <tr><td>2023-06-26   <td>1.0     <td>        <td>
+ * </table>
+ */
+
+#ifndef SE_DEFINES_H
+#define SE_DEFINES_H
+
+
+typedef enum {
+    SUCEESS = 0,
+    
+}SE_ERR;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif  /* SE_DEFINES_H */
